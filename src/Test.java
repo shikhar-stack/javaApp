98243a6dc4355826ecaa913824cd8fa4e6b872df
shikhar-stack <shikharpratapsingh13@gmail.com>
2026-01-04 03:26:45 +0530

    added the req

--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,11 @@
+package src;
 public class Test {
 
 public static void main (String [] srgaStrings){
 
-System.out.print("This is Test Case for app to run ");
+System.out.print("This is Test Case for app to run. --> this is shikhar  ");
 
 
 } 
-
-
 
 }
